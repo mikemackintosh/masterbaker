c843056aa3597a3f53280b9fe81b9e4b596f4292
Mike Mackintosh <m@zyp.io>
2014-10-19 17:52:51 -0400

    fixing dropbox

--- site_cookbooks/apps/recipes/dropbox.rb
+++ site_cookbooks/apps/recipes/dropbox.rb
@@ -1,1 +1,6 @@
-homebrew_cask "dropbox"
+dmg_package 'Dropbox' do
+  volumes_dir 'Dropbox Installer'
+  source      'https://www.dropbox.com/download?src=index&plat=mac'
+  checksum    '9a7b04fb81501b07556afe4beac05d0794d66c9c'
+  action      :install
+end
